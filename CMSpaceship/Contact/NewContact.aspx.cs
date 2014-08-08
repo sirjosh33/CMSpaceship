28de326fa830aaf31bac2f75b4030c51058fbda0
Josh <joshuadkrueger@gmail.com>
2014-08-07 22:16:49 -0500

    8-6-14

--- CMSpaceship/Contact/NewContact.aspx.cs
+++ CMSpaceship/Contact/NewContact.aspx.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Ion.aspnet.Binding;
 
 namespace CMSpaceship.Contact
 {
-    public partial class NewContact : System.Web.UI.Page
+    public partial class NewContact : BindablePage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
---
+++ CMSpaceship/Context/ReusableContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Capsule.Data;
+
+
+namespace CMSpaceship.Context
+{
+    public class ReusableContext
+    {
+        public ReusableContext() 
+        {
+
+        }
+    }
+}
